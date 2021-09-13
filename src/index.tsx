fc7f83028c1ae1f01e42eb910ce12cdb051552cb
Jiayin Li <jiayin.li007@outlook.com>
2021-09-13 13:27:45 -0400

    created basic folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,24 @@
-const x = 0;
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Game, Home } from './screens';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            {/* <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" /> */}
+            <Home />
+            <Game />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
