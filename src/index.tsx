88b2062af2e720e447f64a48a601debc133d474a
Jiayin Li <jiayin.li007@outlook.com>
2021-09-13 18:12:29 -0400

    applied Google Fonts with Expo

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { Game, Home } from '@screens';
+import { StyleSheet, Text, View } from 'react-native';
+import {
+   useFonts,
+   DeliusUnicase_400Regular,
+   DeliusUnicase_700Bold,
+} from '@expo-google-fonts/delius-unicase';
+import AppLoading from 'expo-app-loading';
+// import { Game, Home } from '@screens';
 // import Navigaor from '@config/navigator';
 
 export default function App() {
+   const [fontLoaded] = useFonts({
+      DeliusUnicase_400Regular,
+      DeliusUnicase_700Bold,
+   });
+   if (!fontLoaded) return <AppLoading />;
    return (
       <View style={styles.container}>
          {/* <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" /> */}
+            <StatusBar style="auto" />
          <Image source={require('@assets/icon.png')} />
-         {/* <Home />
+         <Home />
          <Game /> */}
+
+         <Text style={{ fontSize: 25, fontFamily: 'DeliusUnicase_400Regular' }}>
+            Hello World
+         </Text>
       </View>
    );
 }
