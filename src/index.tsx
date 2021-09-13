305dab685c7fdbfcbfe4d5a3446d852330c3b3b8
Jiayin Li <jiayin.li007@outlook.com>
2021-09-13 16:02:16 -0400

    set module resolver

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Game, Home } from './screens';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Game, Home } from '@screens';
+// import Navigaor from '@config/navigator';
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            {/* <Text>Open up App.tsx to start working on your app!</Text>
+   return (
+      <View style={styles.container}>
+         {/* <Text>Open up App.tsx to start working on your app!</Text>
             <StatusBar style="auto" /> */}
-            <Home />
-            <Game />
-        </View>
-    );
+         <Image source={require('@assets/icon.png')} />
+         {/* <Home />
+         <Game /> */}
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
 });
