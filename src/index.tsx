a99ca805101b04db14dcdb6913a5e7bd7d73e52c
Jiayin Li <jiayin.li007@outlook.com>
2021-09-13 20:25:23 -0400

    created a component for Bootstrapping purpose

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,25 @@
 // import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { StyleSheet, View } from 'react-native';
-import {
-   useFonts,
-   DeliusUnicase_400Regular,
-   DeliusUnicase_700Bold,
-} from '@expo-google-fonts/delius-unicase';
-import AppLoading from 'expo-app-loading';
-import { Text } from '@components';
+
+import { Text, AppBootstrap } from '@components';
 // import { Game, Home } from '@screens';
 // import Navigaor from '@config/navigator';
 
-export default function App() {
-   const [fontLoaded] = useFonts({
-      DeliusUnicase_400Regular,
-      DeliusUnicase_700Bold,
-   });
-   if (!fontLoaded) return <AppLoading />;
+export default function App(): ReactElement {
+   // if (!fontLoaded) return <AppLoading />;
    return (
-      <View style={styles.container}>
-         {/* <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
-         <Image source={require('@assets/icon.png')} />
-         <Home />
-         <Game /> */}
-
-         <Text
-            onPress={() => alert(true)}
-            style={{ fontSize: 25 }}
-            // weight="700"
-         >
-            Hello World <Text weight="400">test</Text>
-         </Text>
-      </View>
+      <AppBootstrap>
+         <View style={styles.container}>
+            <Text
+               onPress={() => alert(true)}
+               style={{ fontSize: 25 }}
+               // weight="700"
+            >
+               Hello World <Text weight="400">test</Text>
+            </Text>
+         </View>
+      </AppBootstrap>
    );
 }
 
