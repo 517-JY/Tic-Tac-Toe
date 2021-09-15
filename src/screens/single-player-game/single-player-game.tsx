ce87c22cbe4507b8087e9b5a7ef51761e3d626e3
Jiayin Li <jiayin.li007@outlook.com>
2021-09-15 12:30:26 -0400

    created the Board component

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -2,12 +2,19 @@ import { GradientBackground } from '@components';
 import React, { ReactElement } from 'react';
 import { Text, SafeAreaView } from 'react-native';
 import styles from './single-player-game.styles';
+import { Board } from '@components';
 
 export default function SinglePlayerGame(): ReactElement {
    return (
       <GradientBackground>
          <SafeAreaView style={styles.container}>
-            <Text style={{ color: '#fff' }}>Game</Text>
+            <Board
+               onCellPressed={(index) => {
+                  alert(index);
+               }}
+               state={['x', 'o', null, 'x', 'o', null, 'x', 'o', null]}
+               size={300}
+            />
          </SafeAreaView>
       </GradientBackground>
    );
