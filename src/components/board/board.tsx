38fe0bb12c982f5c3c15692f440cbf0a81d7ca4a
Jiayin Li <jiayin.li007@outlook.com>
2021-09-16 22:45:49 -0400

    created/styled the winning line component

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -1,13 +1,15 @@
 import React, { ReactElement } from 'react';
 import { View, TouchableOpacity } from 'react-native';
 import Text from '../text/text';
-import { BoardState } from '@utils';
+import { BoardResult, BoardState } from '@utils';
+import BoardLine from './board-line';
 
 type Cell = 'x' | 'o' | null;
 type BoardProps = {
    state: BoardState;
    size: number;
    disabled?: boolean;
+   gameResult?: BoardResult | false;
    onCellPressed?: (index: number) => void;
 };
 
@@ -15,6 +17,7 @@ export default function Board({
    state,
    disabled,
    size,
+   gameResult,
    onCellPressed,
 }: BoardProps): ReactElement {
    return (
@@ -46,6 +49,14 @@ export default function Board({
                </TouchableOpacity>
             );
          })}
+
+         {/* Display the BoardLine only if has a gameResult */}
+         {true && (
+            <BoardLine
+               size={size}
+               gameResult={{ winner: 'o', diagonal: 'MAIN', direction: 'D' }}
+            />
+         )}
       </View>
    );
 }
