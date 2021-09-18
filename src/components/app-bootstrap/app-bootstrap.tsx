110b062d833190b537ce927d4c2fcffecf162d7d
Jiayin Li <jiayin.li007@outlook.com>
2021-09-17 23:54:32 -0400

    created a React context(AuthContext) to store logged-in user info

--- src/components/app-bootstrap/app-bootstrap.tsx
+++ src/components/app-bootstrap/app-bootstrap.tsx
@@ -1,4 +1,12 @@
-import React, { ReactElement, ReactNode, useEffect, useState } from 'react';
+import React, {
+   createContext,
+   ReactElement,
+   ReactNode,
+   useEffect,
+   useState,
+   Dispatch,
+   SetStateAction,
+} from 'react';
 import { View, Text } from 'react-native';
 
 import {
@@ -13,6 +21,13 @@ type AppBootstrapProps = {
    children: ReactNode;
 };
 
+type AuthContextType = {
+   user: { [key: string]: any } | null;
+   setUser: Dispatch<SetStateAction<{ [key: string]: any } | null>>;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
 export default function AppBootstrap({
    children,
 }: AppBootstrapProps): ReactElement {
@@ -23,14 +38,17 @@ export default function AppBootstrap({
 
    // Check user authentication status
    const [authLoaded, setAuthLoaded] = useState(false);
+   const [user, setUser] = useState<{ [key: string]: any } | null>(null);
 
    useEffect(() => {
       async function checkCurrentUser() {
          try {
             const user = await Auth.currentAuthenticatedUser();
-            console.log('The user is ' + user);
+            // console.log('The user is ' + user);
+            setUser(user);
          } catch (error) {
-            console.log(error);
+            // console.log(error);
+            setUser(null);
          }
 
          // console.log('Whether is here on AppBootstrap');
@@ -40,5 +58,11 @@ export default function AppBootstrap({
       checkCurrentUser();
    }, []);
 
-   return fontLoaded && authLoaded ? <>{children}</> : <AppLoading />;
+   return fontLoaded && authLoaded ? (
+      <AuthContext.Provider value={{ user, setUser }}>
+         {children}
+      </AuthContext.Provider>
+   ) : (
+      <AppLoading />
+   );
 }
